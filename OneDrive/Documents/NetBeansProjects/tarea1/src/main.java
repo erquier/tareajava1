b426cbac4fbfa3879c626085421e9ac86f12deef
erquier <ernest324@gmail.com>
2017-10-03 22:46:23 -0400

    Tarea1

---
+++ OneDrive/Documents/NetBeansProjects/tarea1/src/main.java
@@ -0,0 +1,24 @@
+
+
+public class main {
+
+   
+    public static void main(String[] args) {
+       
+        System.out.println("Hola usuario bienvenido");
+
+        String dia = "Miecoles";
+        System.out.println("Hoy es " + dia); 
+
+        int hora = 7;
+
+        System.out.println("Son las " + hora + " en punto");
+
+        System.out.println("Dentro de 2 horas serán las " + (hora + 2) + " en punto");
+        
+        
+        
+        
+    }
+    
+}
